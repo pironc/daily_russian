54ab93b2134932180063234a22bee238bdc56a98
sip <sip@sip.local>
2026-05-25 10:32:18 +0200

    Add DailyRussian Xcode SwiftUI project

---
+++ DailyRussian/DailyRussian/DailyRussianApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyRussianApp.swift
+//  DailyRussian
+//
+//  Created by sip on 25/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyRussianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
